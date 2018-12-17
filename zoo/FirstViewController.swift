7c4590c2f2bef78955f86619e472056f2b9de868
Ning Lu <jiangxiluning@gmail.com>
2018-12-17 15:37:46 +0800

    Initial Commit

---
+++ zoo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  zoo
+//
+//  Created by luning on 2018/12/17.
+//  Copyright © 2018 luning. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ zoo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  zoo
+//
+//  Created by luning on 2018/12/17.
+//  Copyright © 2018 luning. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
